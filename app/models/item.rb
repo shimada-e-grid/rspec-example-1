7a911ee1adc76a258baceb91fe461e6fd22ff571
shimada <shimada@e-grid.co.jp>
2023-04-13 00:01:06 +0900

    update

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :note, presence: true, length: { maximum: 20 }
 end
