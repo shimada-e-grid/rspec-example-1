ecc67fe13ec34ed569af0559e0628ebbc4538bae
shimada <shimada@e-grid.co.jp>
2023-04-13 16:17:48 +0900

    update

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,19 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  # 変数を宣言
   let(:item){ build(:item) }
 
+  # バリデーションのテスト
   describe 'validation' do
+    # 名前カラム
     describe 'name' do
+      # 条件
       describe '必須入力' do
         context '入力されている場合' do
           it '有効' do
+            # 実行結果
             expect(item).to be_valid
           end
         end
         context '未入力の場合' do
           it '無効' do
             item.name = ''
+            # 実行結果
             expect(item).to be_invalid
           end
         end
@@ -36,14 +42,18 @@ RSpec.describe Item, type: :model do
     end
   end
 
+  # sampleメソッドのテスト
   describe '#sample' do
+    # テスト毎に実行する処理
     before do
       item.sample
     end
     it 'nameがサンプルであること' do
+      # 実行結果
       expect(item.name).to eq 'サンプル'
     end
-    it '有効であること' do
+    it 'バリデーションが有効であること' do
+      # 実行結果
       expect(item).to be_valid
     end
   end
